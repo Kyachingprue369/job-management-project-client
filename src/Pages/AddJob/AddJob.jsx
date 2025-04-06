0104e1445d27bef8654a93d5833a2ce6d407d72c
kyachingprue <kyachingpruemarma.info@gmail.com>
2025-04-06 14:54:21 +0600

    new commitment

--- src/Pages/AddJob/AddJob.jsx
+++ src/Pages/AddJob/AddJob.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 
 const AddJob = () => {
+
+  const handleAddJob = event => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const initialData = Object.fromEntries(formData.entries());
+    console.log(initialData);
+    const { min, max, currency, ...newJob } = initialData;
+    console.log(newJob);
+    newJob.salaryRange = { min, max, currency };
+  }
   return (
     <div className='w-10/12 mx-auto'>
       <h3 className='text-center my-4 md:my-10 text-2xl md:text-4xl font-bold'>Add Job </h3>
-      <form>
+      <form onSubmit={handleAddJob}>
         <div className='my-4 md:flex items-center gap-6'>
           <div className=' md:flex-1'>
             <label className="fieldset-label pb-2">Job Type</label>
-            <select defaultValue="Pick a color" className="select w-full" required>
+            <select defaultValue="Pick a color" name='jobType' className="select select-info w-full" required>
               <option disabled={true}>Pick a Job</option>
               <option>Full-time</option>
               <option>Intern</option>
@@ -19,9 +29,9 @@ const AddJob = () => {
             </select>
           </div>
           <div className='md:flex-1'>
-            <label className="fieldset-label pb-2">Job Field/Title</label>
-            <select defaultValue="Pick a color" className="select w-full" required>
-              <option disabled={true}>Pick a Job field</option>
+            <label className="fieldset-label pb-2">Job Title</label>
+            <select defaultValue="Pick a color" name='title' className="select select-info w-full" required>
+              <option disabled={true}>Pick a Job Title</option>
               <option>Software Engineer</option>
               <option>Marketing Specialist</option>
               <option>Finance Manager</option>
@@ -47,11 +57,11 @@ const AddJob = () => {
         <div className='my-4 md:flex items-center gap-6'>
           <div className='md:flex-1'>
             <label className="fieldset-label pb-2">Location</label>
-            <input type="text" className="input w-full" name='location' placeholder="Enter you location" required />
+            <input type="text" className="input input-info w-full" name='location' placeholder="Enter you location" required />
           </div>
           <div className='md:flex-1'>
             <label className="fieldset-label pb-2">Job Category</label>
-            <select defaultValue="Pick a color" className="select w-full" required>
+            <select defaultValue="Pick a color" name='category' className="select select-info w-full" required>
               <option disabled={true}>Pick a Job Category</option>
               <option>Development</option>
               <option>Engineering</option>
@@ -68,22 +78,22 @@ const AddJob = () => {
         {/* 3rd step */}
         <div className='mb-5'>
           <label className="fieldset-label pb-2">Application Deadline</label>
-          <input type="date" className="input w-full" name='applicationDeadline' placeholder="Email" required />
+          <input type="date" className="input input-info w-full" name='applicationDeadline' placeholder="Email" required />
         </div>
         {/* 4 step */}
         <p className='text-gray-800'>Salary Range</p>
         <div className='mb-5 md:flex items-center gap-4'>
           <div className='md:flex-1'>
             <label className="fieldset-label pb-2">Min</label>
-            <input type="text" className="input" placeholder="min" required />
+            <input type="text" className="input input-info" name='min' placeholder="min" required />
           </div>
           <div className='md:flex-1'>
             <label className="fieldset-label pb-2">Max</label>
-            <input type="text" className="input" placeholder="max" required />
+            <input type="text" className="input input-info" name='max' placeholder="max" required />
           </div>
           <div className='md:flex-1'>
             <label className="fieldset-label pb-2">Currency</label>
-            <select defaultValue="Pick a color" className="select w-full" required>
+            <select defaultValue="Pick a color" name='currency' className="select select-info w-full" required>
               <option disabled={true}>Job currency</option>
               <option>BDT</option>
               <option>USD</option>
@@ -115,7 +125,7 @@ const AddJob = () => {
         {/* 6 step */}
         <div className='my-5'>
           <label className="fieldset-label pb-2">Company Name</label>
-          <input type="text" className="input w-full" name='company' placeholder="Enter you company name" required />
+          <input type="text" className="input input-info w-full" name='company' placeholder="Enter you company name" required />
         </div>
         {/* 7 step */}
         <div className='my-5'>
@@ -130,7 +140,7 @@ const AddJob = () => {
         {/* 9 step */}
         <div className='my-5'>
           <label className="fieldset-label pb-2">Status</label>
-          <select defaultValue="active" className="select w-full" required>
+          <select defaultValue="active" name='status' className="select select-info w-full" required>
             <option disabled={true}>Job Status</option>
             <option>active</option>
             <option>negative</option>
@@ -141,17 +151,17 @@ const AddJob = () => {
         <div className='md:flex items-center my-5 gap-5'>
           <div className='md:flex-1'>
             <label className="fieldset-label pb-2">HR Name</label>
-            <input type="text" className="input w-full" name='hr_name' placeholder="Enter hr name" required />
+            <input type="text" className="input input-info w-full" name='hr_name' placeholder="Enter hr name" required />
           </div>
           <div className='md:flex-1'>
             <label className="fieldset-label pb-2">HR Email</label>
-            <input type="email" className="input w-full" name='hr_email' placeholder="Enter hr email" required />
+            <input type="email" className="input input-info w-full" name='hr_email' placeholder="Enter hr email" required />
           </div>
         </div>
         {/* Final company logo */}
         <div className='mb-5 md:mb-10'>
           <label className="fieldset-label pb-2">Company Logo</label>
-          <input type="url" className="input w-full" name='company_log' placeholder="Enter company logo" required />
+          <input type="url" className="input input-info w-full" name='company_logo' placeholder="Enter company logo" required />
         </div>
         {/* Submit button */}
         <div className='mb-10 md:mb-20'>
